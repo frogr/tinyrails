dac0dd6997c707a8c3efcc1103519dbba37b3799
Austin French <austindanielfrench@gmail.com>
2025-09-06 08:01:03 -0700

    gem init

---
+++ lib/tinyrails.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tinyrails/version"
+
+module Tinyrails
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tinyrails/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Tinyrails
+  VERSION = "0.1.0"
+end
