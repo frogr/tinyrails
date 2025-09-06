f9cebcb1929197cfd29137d3846583091a598281
Austin French <austindanielfrench@gmail.com>
2025-09-06 08:41:07 -0700

    successfully propogating to other project

--- lib/tinyrails.rb
+++ lib/tinyrails.rb
@@ -3,6 +3,11 @@
 require_relative "tinyrails/version"
 
 module Tinyrails
+  class Application
+    def call(env)
+      [200, {"Content-Type" => 'text/html'}, ["Hello from Tinyrails!"]]
+    end
+  end
   class Error < StandardError; end
   # Your code goes here...
 end
