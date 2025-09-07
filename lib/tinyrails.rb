f8cff610bbeef2ce54d8381c7464a2699a3dd83e
Austin French <austindanielfrench@gmail.com>
2025-09-07 10:04:01 -0700

    controller dispatch beginnings

--- lib/tinyrails.rb
+++ lib/tinyrails.rb
@@ -1,12 +1,26 @@
 # frozen_string_literal: true
 
 require_relative 'tinyrails/version'
+require_relative 'tinyrails/routing'
 require_relative 'tinyrails/array'
 
 module Tinyrails
   class Application
     def call(_env)
-      [200, { 'Content-Type' => 'text/html' }, ['Hello from Tinyrails!']]
+      klass, act = get_controller_and_action(_env)
+      controller = klass.new(_env)
+      text = controller.send(act)
+      [200, { 'content-type' => 'text/html' }, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 
