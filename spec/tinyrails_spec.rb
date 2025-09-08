379250a793b0339dca8fc25ec17dd11e9c218a0b
Austin French <austindanielfrench@gmail.com>
2025-09-08 07:18:31 -0700

    rspec

--- spec/tinyrails_spec.rb
+++ spec/tinyrails_spec.rb
@@ -11,10 +11,34 @@ RSpec.describe Tinyrails do
     Tinyrails::Application.new
   end
 
+  class TestsController < Tinyrails::Controller
+    def an_action
+      "Result!"
+    end
+  end
+
   it 'can make a request' do
-    get '/'
+    get '/tests/an_action'
 
     expect(last_response).to be_ok
-    expect(last_response.body).to include('Hello')
+    expect(last_response.body).to include('Result!')
+  end
+
+  context 'when the file doesnt exist' do
+    it 'displays the correct error' do
+      get '/fake_route'
+      expect(last_response).to_not be_ok
+      expect(last_response.status).to eq(404)
+      expect(last_response.body).to include("was not found")
+    end
+  end
+
+  context 'when the action is missing from a controller' do
+    it 'displays the correct error' do
+      get '/tests/fake_action'
+      expect(last_response).to_not be_ok
+      expect(last_response.status).to eq(404)
+      expect(last_response.body).to include("undefined method")
+    end
   end
 end
